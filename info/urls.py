4ab9974e897675ccc2fed4bf1682cc46e4399865
ArtemStar008 <art.starostenko2014@gmail.com>
2023-10-18 23:57:01 +0300

    final version stock django

--- info/urls.py
+++ info/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("two/", views.second_page, name='second_page'),
     path("three/", views.third_page, name='third_page'),
     path("two/three/", views.third_page, name='third_page'),
+    # path("test/", views.MyView.as_view(), name='test'),
+    path("posts/", views.PostsView.as_view(), name='posts'),
+    path("posts/<int:id>", views.PostsView.as_view(), name='post'),
 
 ]
--- info/views.py
+++ info/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+
+from info.models import InfoBlog
 
 
 # Create your views here.
@@ -14,3 +17,23 @@ def second_page(request):
 
 def third_page(request):
     return render(request, 'third_page.html')
+
+
+class PostsView(View):
+    def get(self, request, id=None):
+        posts = InfoBlog.objects.filter(is_deleted=False)  # запрос подготовился
+
+        if id:
+            post = InfoBlog.objects.filter(id=id, is_deleted=False).first()
+            return render(request, 'post.html', context={'post': post, 'posts': posts})
+
+        return render(request, 'db_admin.html', context={'posts': posts})
+
+        # # print(posts.query)
+        # # print(amount_posts)
+        # return render(request, 'db_admin.html', context={
+        #     # 'amount_posts': posts,
+        #     'posts': posts,
+        # })  # запрос пошел в базу
+
+# {'amount_posts_name': amount_posts_value}
